086efd14289f769902f697b03ccd49ff94f6474e
2387923 <yassineeljal0@gmail.com>
2024-04-25 16:44:17 -0400

    save

--- src/main/java/Projet/Animaux.java
+++ src/main/java/Projet/Animaux.java
@@ -3,5 +3,23 @@ package Projet;
 import java.util.ArrayList;
 
 public class Animaux {
+    protected int age;
+    protected int esperenceVie;
+    protected String type;
 
+    public Animaux(int age, int esperenceVie, String type) {
+        this.age = age;
+        this.esperenceVie = esperenceVie;
+        this.type = type;
+    }
+
+
+    public boolean estMort(){
+        if (age > esperenceVie){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
 }
--- src/main/java/Projet/Lion.java
+++ src/main/java/Projet/Lion.java
@@ -1,5 +1,31 @@
-package Projet;
+//package Projet;
+import javafx.scene.control.Label;
+import javafx.application.Application;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
 
-public class Lion {
+import java.util.ArrayList;
 
-}
+//public class Lion extends Application {
+   // TextField lion = new TextField();
+    //lion.set
+    //public Lion(int age, int esperenceVie, String type) {
+      //  super(0, 12, "carnivore");
+    //}
+    //for(i=0;i < lion;i++){
+    //    Lion lion = new Lion(12, 0, "carnivore");
+   // }
+
+    //@Override
+    //public void start(Stage stage) throws Exception {
+
+  //  }
+//}
